e69aa853fc48aa0a2f546825ee5819ddc0613b70
KeshavChromeOS <nknnkumar@yahoo.com>
2023-02-15 02:11:22 +0530

    keshav

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 const os = require('os');
 
 let CommonForWin32 = require("./kcode/ForWin32");
+let CommonForLinux = require("./kcode/ForLinux");
 
 let CommonOsName = os.platform();
 
@@ -8,6 +9,10 @@ switch (CommonOsName) {
     case "win32":
         CommonForWin32.StartFunc();
         break;
+
+case "linux":
+    CommonForLinux.StartFunc();
+    break;
 
     default:
         break;

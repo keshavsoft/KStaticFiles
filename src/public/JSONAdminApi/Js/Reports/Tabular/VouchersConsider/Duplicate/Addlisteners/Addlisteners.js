3f6b00f32acbf956d1a94dda793239e468508596
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-02-18 19:53:31 +0530

    SREENIVAS

---
+++ src/public/JSONAdminApi/Js/Reports/Tabular/VouchersConsider/Duplicate/Addlisteners/Addlisteners.js
@@ -0,0 +1,53 @@
+let jFCloneFunc = () => {
+    let jVarLocalCloneClassName = document.getElementsByClassName("CloneButtonClass");
+
+    for (let i = 0; i < jVarLocalCloneClassName.length; i++) {
+        jVarLocalCloneClassName[i].addEventListener("click", jFLocalClickFunc)
+    };
+
+};
+let jFLocalClickFunc = async (event) => {
+    let jVarLocalCurrentTarget = event.currentTarget;
+    let jVarLocalreportName = jVarLocalCurrentTarget.dataset.reportname;
+    let jVarLocalpk = jVarLocalCurrentTarget.dataset.pk;
+
+
+    let jFetchUrl = "/JSONAdminApi/AdminApi/AsTree/Json/UserFolders/ReportsFolder/LedgerAutoJsonFile/FromReports/VouchersConsider";
+
+    let jVarLocalRequestHeader = {
+        method: "PATCH",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            ReportName: jVarLocalreportName,
+            pk: jVarLocalpk
+        })
+    };
+
+    let response = await fetch(jFetchUrl, jVarLocalRequestHeader);
+    // console.log("response.status", response.status);
+
+    // const ResponseData = await response.json();
+
+
+    switch (response.status) {
+        case 200:
+            // Swal.fire('Dublicated')
+            let jVarLocalNewLocation = "";
+            jVarLocalNewLocation += `?inReportName=${jVarLocalreportName}`
+            jVarLocalNewLocation += `&inRowPK=${jVarLocalpk}`
+            window.location = jVarLocalNewLocation;
+
+            break;
+
+        case 204:
+            Swal.fire('Not Update Data')
+            break;
+        default:
+        // code block
+    };
+};
+
+export { jFCloneFunc };

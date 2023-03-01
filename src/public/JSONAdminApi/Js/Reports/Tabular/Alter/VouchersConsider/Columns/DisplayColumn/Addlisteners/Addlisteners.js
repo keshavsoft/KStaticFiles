9fc6f82a8b1b1490c45b77bdaf718bfa74b17797
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-03-01 19:31:46 +0530

    SRENIVAS

--- src/public/JSONAdminApi/Js/Reports/Tabular/Alter/VouchersConsider/Columns/DisplayColumn/Addlisteners/Addlisteners.js
+++ src/public/JSONAdminApi/Js/Reports/Tabular/Alter/VouchersConsider/Columns/DisplayColumn/Addlisteners/Addlisteners.js
@@ -13,16 +13,19 @@ let jFLocalClickFunc = async (event) => {
     let jVarLocalColumnPk = jVarLocalCurrentTarget.dataset.columnpk;
 
     let jVarLocalColsestTr = jVarLocalCurrentTarget.closest("tr");
+    let jVarLocalName = jVarLocalColsestTr.querySelector('[name="Name"]');
     let jVarLocalDisplayColumn = jVarLocalColsestTr.querySelector('[name="DisplayColumn"]');
     let jVarLocalTransformType = jVarLocalColsestTr.querySelector('[name="TransformType"]');
     let jVarLocalDefaultValue = jVarLocalColsestTr.querySelector('[name="DefaultValue"]');
 
+    let jVarLocalNameValue = jVarLocalName.value;
     let jVarLocalDisplayColumnValue = jVarLocalDisplayColumn.value;
     let jVarLocalTransformTypeValue = jVarLocalTransformType.value;
     let jVarLocalDefaultValueValue = jVarLocalDefaultValue.value;
 
 
     let BodyAsJson = {
+        Name: jVarLocalNameValue,
         DisplayColumn: jVarLocalDisplayColumnValue,
         TransformType: jVarLocalTransformTypeValue,
         DefaultValue: jVarLocalDefaultValueValue,

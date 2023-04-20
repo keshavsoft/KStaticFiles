79f6f1d42e8da3a039cfa83c5a0df75567500d17
keshavsoft <nknnkumar@yahoo.com>
2023-04-20 17:28:33 +0530

    keshav

---
+++ src/public/JSONAdminApi/Js/Login/LoginFetch.js
@@ -0,0 +1,53 @@
+const StartFunc = () => {
+    let jVarLocalLoginModalButtonId = document.getElementById("LoginModalButtonId");
+
+    jVarLocalLoginModalButtonId.addEventListener("click", jFLocalLoginButtonClick);
+};
+
+const jFLocalLoginButtonClick = () => {
+    let jVarLocalObject = {};
+
+    jVarLocalObject.UserName = document.getElementById("KUserNameInput").value;
+    jVarLocalObject.Password = document.getElementById("KPasswordInput").value;
+
+    if (jVarLocalObject.UserName !== "" && jVarLocalObject.Password !== "") {
+        jVarLocalCheckUserNamePassword({ inUserName: jVarLocalObject.UserName, inPassword: jVarLocalObject.Password });
+    };
+};
+
+const jVarLocalCheckUserNamePassword = async ({ inUserName, inPassword }) => {
+    let jVarLocalUserCheckRoute = "JSONAdminApi";
+    let jVarLocalFetchUrl = `/${jVarLocalUserCheckRoute}/Validate/Users/InUserDataJson/ForUserNameAndPassword/TokenToCookie`;
+
+    let response = await fetch(jVarLocalFetchUrl, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ inUserName, inPassWord: inPassword })
+    });
+    let FetchDataJson = await response.json();
+
+    if (FetchDataJson !== null) {
+        if (FetchDataJson.KTF) {
+            var myModalEl = document.getElementById('LoginModalId');
+
+            var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
+
+            modal.hide();
+
+            jFLocalFirmDetails({ inUserName, inFirmDetails: FetchDataJson });
+            this.Login.ClientSideCookieFuncs.CheckToken();
+        } else {
+            document.getElementById("KUserNameInput").classList.add("is-invalid");
+        };
+    };
+};
+
+const jFLocalFirmDetails = ({ inUserName, inFirmDetails }) => {
+    localStorage.setItem("kUserName", inUserName);
+    localStorage.setItem("FirmDetails", JSON.stringify(inFirmDetails));
+};
+
+StartFunc();

cc7ed18de4e46ca20bfe045a3f3f56ffc476a34b
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-04-25 12:46:21 +0530

    SREENIVAS

--- src/public/JSONAdminApi/Js/Columns/Tabular/Duplicate/DuplicateItem.js
+++ src/public/JSONAdminApi/Js/Columns/Tabular/Duplicate/DuplicateItem.js
@@ -1,6 +1,9 @@
 import { jFUpdateFunc } from "./Item/Addlisteners.js";
 import { jFCreateFoldersToDom } from "./Item/ShowOnDom.js";
+import ApiConfigJson from '../ApiConfig.json' assert {type: 'json'};
 
-jFCreateFoldersToDom().then(FromjFCreateFoldersToDom => {
-    jFUpdateFunc();
+let jVarCommonProjectName = ApiConfigJson.ProjectName;
+
+jFCreateFoldersToDom({inProjectName: jVarCommonProjectName}).then(FromjFCreateFoldersToDom => {
+    jFUpdateFunc({inProjectName: jVarCommonProjectName});
 });

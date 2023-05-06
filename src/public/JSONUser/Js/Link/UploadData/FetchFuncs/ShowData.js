0df04ff6b0ac3733f78459671a8714a7bc5c6d25
Keshav <nknnkumar@yahoo.com>
2023-05-06 21:21:18 +0530

    keshav

--- src/public/JSONUser/Js/Link/UploadData/FetchFuncs/ShowData.js
+++ src/public/JSONUser/Js/Link/UploadData/FetchFuncs/ShowData.js
@@ -11,14 +11,14 @@ let StartFunc = async () => {
 let LocalShowDataFunc = async ({ inData }) => {
     let jVarLocalRawTemplate = document.getElementById("HbsTemplateForBody").innerHTML;
 
-    Object.entries(inData).forEach(
-        ([key, value]) => {
-            if (("UserName" in value) === false) {
-                value.RowClass = "table-primary";
-                value.ShowInsertNew = true;
-            };
-        }
-    );
+    // Object.entries(inData).forEach(
+    //     ([key, value]) => {
+    //         if (("UserName" in value) === false) {
+    //             value.RowClass = "table-primary";
+    //             value.ShowInsertNew = true;
+    //         };
+    //     }
+    // );
 
     let jVarGlobalPresentViewData = inData;
     document.getElementById("KTableBodyId").innerHTML = Handlebars.compile(jVarLocalRawTemplate)(jVarGlobalPresentViewData);

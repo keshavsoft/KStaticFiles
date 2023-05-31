9d80614659289c4f7743277aa07c11301a5a9c0d
keshavsoft <nknnkumar@yahoo.com>
2023-05-31 16:19:57 +0530

    keshav

---
+++ CopyFiles.bat
@@ -0,0 +1,12 @@
+xcopy public\Projects\ClientLogin ..\JsonApi\public\ClientLogin\  /s /e /Y
+xcopy public\Projects\JSONAdminApi  ..\JsonApi\public\JSONAdminApi\  /s /e /Y
+xcopy public\Projects\JSONReports  ..\JsonApi\public\JSONReports\  /s /e /Y
+xcopy public\Projects\JSONUser  ..\JsonApi\public\JSONUser\  /s /e /Y
+xcopy public\Projects\JSONUtility  ..\JsonApi\public\JSONUtility\  /s /e /Y
+
+xcopy src\public\ClientLogin ..\JsonApi\public\ClientLogin\  /s /e /Y
+xcopy src\public\JSONAdminApi  ..\JsonApi\public\JSONAdminApi\  /s /e /Y
+xcopy src\public\JSONReports  ..\JsonApi\public\JSONReports\  /s /e /Y
+xcopy src\public\JSONUser  ..\JsonApi\public\JSONUser\  /s /e /Y
+xcopy src\public\JSONUtility  ..\JsonApi\public\JSONUtility\  /s /e /Y
+xcopy src\public\Garments   ..\JsonApi\public\Garments \  /s /e /Y

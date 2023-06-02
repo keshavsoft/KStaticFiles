5e26305cf384beeb50cd0eac08104598a94258ab
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-06-02 19:33:00 +0530

    SREENIVAS

---
+++ src/public/JSONAdminApi/Js/Admin/CreateItem/FormLoad/Addlisteners.js
@@ -0,0 +1,19 @@
+import { StartFunc as StartFuncCreateFolderButton } from "./ListenerFuncs/CreateFolderButton.js";
+
+let StartFunc = ({ inProjectName }) => {
+    let jVarLocalFileCreateClassName = document.getElementsByClassName("CreateFileButtonClass");
+
+
+    for (let i = 0; i < jVarLocalFileCreateClassName.length; i++) {
+        jVarLocalFileCreateClassName[i].addEventListener("click", async (event) => {
+            await StartFuncCreateFolderButton({
+                inEvent: event,
+                inProjectName
+            });
+        });
+    };
+
+  
+};
+
+export { StartFunc };

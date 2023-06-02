c0d6fc7071849feaa06161d831ccd6a12a31ea93
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-02 18:34:54 +0530

    keshav

--- src/public/JSONUser/Js/LoginToDashboard/PostShow/LoginButton/PostFetch.js
+++ src/public/JSONUser/Js/LoginToDashboard/PostShow/LoginButton/PostFetch.js
@@ -1,6 +1,6 @@
 let StartFunc = async ({ inFromFetch }) => {
     if ("RedirectPage" in inFromFetch) {
-        window.open(LocalFromFetch.RedirectPage, '_newtab');
+        window.open(inFromFetch.RedirectPage, '_newtab');
         //window.location.href = LocalFromFetch.RedirectPage;
     };
 

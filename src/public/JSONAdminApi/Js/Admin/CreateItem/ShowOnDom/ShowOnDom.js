c6465aa853c20a1bcbcaf348cca16fa2c2ef9812
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-06-03 18:50:36 +0530

    SREENIVAS

--- src/public/JSONAdminApi/Js/Admin/CreateItem/ShowOnDom/ShowOnDom.js
+++ src/public/JSONAdminApi/Js/Admin/CreateItem/ShowOnDom/ShowOnDom.js
@@ -1,3 +1,5 @@
+import { StartFunc as RowAndColumnsStartFunc } from "./ApplyClasses.js";
+
 let jFCreateFoldersToDom = async ({ inProjectName }) => {
     let jVarLocalRoute = inProjectName;
     let jVarLocalFetchUrl = `/${jVarLocalRoute}/AdminApi/AsTree/Json/UserFolders/ConfigFolder/UserFileAsFolder/JsonItem/Duplicate/DuplicateItem`;
@@ -6,6 +8,8 @@ let jFCreateFoldersToDom = async ({ inProjectName }) => {
     let dataFromApi = await jVarLocalFromFetch.json();
 
     if (dataFromApi !== null) {
+        RowAndColumnsStartFunc({inDataFromApi:dataFromApi});
+
         let jVarLocalRawTemplate = document.getElementById("HbsTemplateForFoldersOnly").innerHTML;
         document.getElementById("KCont1").innerHTML = Handlebars.compile(jVarLocalRawTemplate)(dataFromApi);
     };
